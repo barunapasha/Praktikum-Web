23c0c858a3194bee84d6bbdf7c052acdf841d8ea
barunapasha <baruna.pasha98@gmail.com>
2024-12-04 11:54:02 +0700

    Latihan Week-7

---
+++ process.php
@@ -0,0 +1,54 @@
+<?php
+  $host = 'localhost';
+  $username = 'root';
+  $password = '';
+  $database = 'test';
+
+  $error = '';
+  $isError = false;
+
+//   $conn = new mysqli($host, $username, $password, $database);
+//   if ($conn->connect_error) {
+//     die("Koneksi gagal: " . $conn->connect_error);
+//   } 
+
+  if(isset($_POST['name']) && $_POST['name'] !== '') {
+    if(isset($_POST['nim']) && $_POST['nim'] !== '') {
+      $error = '';
+      $isError = false;
+    } else {
+      $error = 'NIM belum diisi';
+      $isError = true;
+    }
+  } else {
+    $error = 'Nama belum diisi';
+    $isError = true;
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php if($isError): ?>
+    <p><?= $error ?></p> 
+  <?php endif; ?>
+  <?php if($_POST['name']): ?>
+    <h1><?= $_POST['name'] ?></h1>
+  <?php endif ?>
+  <form action="" method='post'>
+    <div>
+      <label for="nama">Nama: </label>
+      <input id="nama" name="nama" />
+    </div>
+    <div>
+      <label for="nim">Nim: </label>
+      <input id="nim" nim="nim" />
+    </div>
+    <button type="submit">submit</button>
+  </form>
+</body>
+</html>
